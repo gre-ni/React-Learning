196668b1ee0e1ecf74be0af2d59a9518f26c274b
NikolG <greplova.nikol@gmail.com>
2026-04-08 19:17:30 +0200

    global state and context

---
+++ global-state/src/Component4.jsx
@@ -0,0 +1,47 @@
+import "./Component4.css";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+export const Component4 = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div className="Component4">
+      <p>Začátek komponenty Component4.</p>
+      {user !== null ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Jméno</th>
+              <td>
+                {user.firstName} {user.lastName}
+              </td>
+            </tr>
+            <tr>
+              <th>Věk</th>
+              <td>{user.age}</td>
+            </tr>
+            <tr>
+              <th>E-mail</th>
+              <td>{user.email}</td>
+            </tr>
+            <tr>
+              <th>Telefon</th>
+              <td>{user.phone}</td>
+            </tr>
+            <tr>
+              <th>Adresa</th>
+              <td>
+                {user.address.street} {user.address.streetNumber},{" "}
+                {user.address.zipCode} {user.address.streetCity}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <p>Aktuálně není přihlášen žádný uživatel.</p>
+      )}
+      <p>Konec komponenty Component4.</p>
+    </div>
+  );
+};
